238d2b97fd914f2917e24f53344cb06d39970856
unknown <m.a.l.prasetya@students.uu.nl>
2024-07-10 13:22:49 +0200

    changed model path

--- llm4spi.py
+++ llm4spi.py
@@ -1,4 +1,4 @@
 from gpt4all import GPT4All
-model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", device='gpu') # downloads / loads a 4.66GB LLM
+model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf", model_path="~/models") # downloads / loads a 4.66GB LLM
 with model.chat_session():
     print(model.generate("Generate a python function that takes two inputs A and B, and returns whether A is less than B", max_tokens=1024))
